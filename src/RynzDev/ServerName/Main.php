6c44b869ca02974c656ef3aae0635d41ab3e649f
RynzDev <101009914+RynzDev@users.noreply.github.com>
2022-04-03 00:16:18 +0700

    Add files via upload

--- src/RynzDev/ServerName/Main.php
+++ src/RynzDev/ServerName/Main.php
@@ -1,1 +1,11 @@
+<?php
+namespace RynzDev\ServerName;
 
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+    public function onEnable(): void
+    {
+        $this->getServer()->getNetwork()->setName($this->getConfig()->get("custom-name"));
+    }
+}
